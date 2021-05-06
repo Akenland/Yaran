07d83a2007de692597c31ac4da5cc9f4c2046ebf
Kyle Nanakdewa <KyleN@live.ca>
2021-05-06 01:07:42 -0400

    revert exponent/sigmoid function change

--- src/com/kylenanakdewa/yaran/generation/YaranNoiseGenerator.java
+++ src/com/kylenanakdewa/yaran/generation/YaranNoiseGenerator.java
@@ -116,6 +116,9 @@ public class YaranNoiseGenerator {
      * @return the adjusted value
      */
     private double adjustNoise(double noise) {
+        // Raise noise to a power (redistribution)
+        noise = Math.pow(noise, exponent);
+
         // Sigmoid function to create plateaus
         if (sigmoidMultiplier != 0) {
             if (sigmoidScale != 0 && sigmoidScale != 1) {
@@ -124,9 +127,6 @@ public class YaranNoiseGenerator {
                 noise = YaranMath.sigmoid(noise, sigmoidMultiplier);
             }
         }
-
-        // Raise noise to a power (redistribution)
-        noise = Math.pow(noise, exponent);
 
         return noise;
     }
